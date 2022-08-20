26d606560906b6d1c954764980266f0da9da36a2
Joshua Ramat <ramatjoshua@gmail.com>
2022-08-20 12:39:14 -0700

    set up PostCard component

---
+++ components/PostCard.js
@@ -0,0 +1,15 @@
+const styles = {
+  authorContainer: ``,
+  authorImageContainer ``,
+  
+}
+
+const PostCard = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default PostCard
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import PostCard from '../components/PostCard'
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <PostCard />
     </>
   )
 }
