04fe82b657d6df97c3178614111a014dc60cbff3
Joshua Ramat <ramatjoshua@gmail.com>
2022-08-24 14:54:13 -0700

    completed postCard component

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import Logo from './../static/banner.png'
+import { FiBookmark } from 'react-icons/fi'
 
 const styles = {
+  wrapper:`flex max-width-[46rem] h-[10rem] items-center gap-[1rem] cursor-pointer`,
   authorContainer: `flex gap-[.4rem]`,
   authorImageContainer: `grid place-items-center rounded-full overflow-hidden h-[1.4rem] w-[1.4rem]`,
   authorName: `font-semibold`,
@@ -11,31 +13,46 @@ const styles = {
   articleDetails: `my-2 text-[.8rem] `,
   detailsContainer:`flex items-center justify-between text-[#787878]`,
   category: `bg-[#f2f3f2] p-1 rounded-full`,
-  postDetails: ``,
+  postDetails: `flex-[2.5] flex flex-col`,
+  bookmarkContainer: `cursor-pointer`,
+  thumbnailContainer:``
 }
 
 const PostCard = () => {
   return (
-    <div className={styles.postDetails}>
-      <div className={styles.authorContainer}>
-        <div className={styles.authorImageContainer}>
-          <Image
-            src={Logo}
-            className={styles.authorImage}
-            width={40}
-            height={40}
-          />
+    <div className={styles.wrapper}>
+      <div className={styles.postDetails}>
+        <div className={styles.authorContainer}>
+          <div className={styles.authorImageContainer}>
+            <Image
+              src={Logo}
+              className={styles.authorImage}
+              width={40}
+              height={40}
+            />
+          </div>
+
+          <div className={styles.authorName}>Joshua Ramat</div>
         </div>
 
-        <div className={styles.authorName}>Joshua Ramat</div>
+        <h1 className={styles.title}>7 Free Tools That Will Make You More Productive In 2022</h1>
+        <div className={styles.briefing}>
+          Productivity is a skill that can be learned.
+        </div>
+        <div className={styles.detailsContainer}>
+          <span className={styles.articleDetails}>Aug 23 • 5 min read • <span className={styles.category}>productivity</span></span>
+          <span className={styles.bookmarkContainer}>
+            <FiBookmark className='h-5 w-5' />
+          </span>
+        </div>
       </div>
 
-      <h1 className={styles.title}>7 Free Tools That Will Make You More Productive In 2022</h1>
-      <div className={styles.briefing}>
-        Productivity is a skill that can be learned.
-      </div>
-      <div className={styles.detailsContainer}>
-        <span className={styles.articleDetails}>Aug 23 • 5 min read • <span className={styles.category}>productivity</span></span>
+      <div className={styles.thumbnailContainer}>
+        <Image
+          height={100}
+          width={100}
+          src={Logo}
+        />
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,27 @@ import Header from '../components/Header'
 import Hero from '../components/Hero'
 import PostCard from '../components/PostCard'
 
+const styles = {
+  postList: `flex flex-col gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3`,
+  container:`max-w-7xl flex-1`,
+  main: `flex justify-center`,
+  wrapper: `mx-auto`
+}
+
 export default function Home() {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
       <Hero />
-      <PostCard />
-    </>
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.postList}>
+            <PostCard />
+            <PostCard />
+            <PostCard />
+          </div>
+        </div>  
+      </div>
+    </div>
   )
 }
